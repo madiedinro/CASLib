89711fc2f880fcc0429dd22b15a9af2b739c0617
Dmitry <madiedinro@gmail.com>
2011-04-02 12:36:11 +0400

    Избавился от конфига на php

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,10 +1,18 @@
 <?php
 
-require_once __DIR__.'/config.php';
 require_once __DIR__.'/../src/Util/Autoloader.php';
 
 use AdvancedContextLib\Util\Autoloader;
 use AdvancedContextLib\Core;
+use Yaml\sfYaml;
+
+$nsMap = array(
+	'AdvancedContextLib' => __DIR__ . '/../src',
+	'Yaml' => __DIR__ . '/../vendor/Yaml',
+);
 
 Autoloader::getInstance()->setMap($nsMap);
+
+$config = sfYaml::load(__DIR__.'/config.yml');
+Core::getInstance()->setParameters($config);
 
